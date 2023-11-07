c03e0e9caa28db013c1a3e75895e6e30d36f3a74
misha <mishanisar96@gmail.com>
2023-11-07 09:19:27 -0800

    project initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route} from "react-router-dom";
+import RegistrationForm from './pages/RegistrationForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      {/* <Route element={<Protectedroutes/>}>
+      <Route path='/' element={<FrontPage />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contactus />} />
+        <Route path='/client' element={<Clientlist />} />
+        <Route path='/newclient' element={<Newclient />} />
+        <Route path='/caselist' element={<Caselist />} />
+        <Route path='/createcase' element={<Createcase />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/chart' element={<Chart />} />
+        <Route path='/login' element={<Login />} />
+     
+        </Route> */}
+        <Route path='/register' element={<RegistrationForm />} />
+    
+      </Routes>
+    </>
   );
 }
 
